80eb2f65401c759d184457c3320d6662565653e9
daniel9992 <greivindaniel1616@gmail.com>
2020-10-16 22:26:28 -0600

    Updates

---
+++ precache-manifest.5783a31d2d4b764763f99a23b2137d17.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83ca1118bfd9f7e8c0c5be708601911e",
+    "url": "/beutiful-nails/index.html"
+  },
+  {
+    "revision": "15957fed5fd794835683",
+    "url": "/beutiful-nails/static/css/main.6e1647d2.chunk.css"
+  },
+  {
+    "revision": "4338daa2a78d26951b0e",
+    "url": "/beutiful-nails/static/js/2.d39246de.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/beutiful-nails/static/js/2.d39246de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15957fed5fd794835683",
+    "url": "/beutiful-nails/static/js/main.2b9f8c27.chunk.js"
+  },
+  {
+    "revision": "c4bc92bba14e05a12ac6",
+    "url": "/beutiful-nails/static/js/runtime-main.3b72df37.js"
+  },
+  {
+    "revision": "5d4709f1b19e4bc1ab976ddd8c99e7e6",
+    "url": "/beutiful-nails/static/media/logo_U_bellas.5d4709f1.jpg"
+  },
+  {
+    "revision": "7d5f29bf703452c1de7721ba9dd6f543",
+    "url": "/beutiful-nails/static/media/s1.7d5f29bf.png"
+  },
+  {
+    "revision": "71e84b5c228c7c4397f5e4a2f01052f2",
+    "url": "/beutiful-nails/static/media/s10.71e84b5c.png"
+  },
+  {
+    "revision": "c47a9407ba0874bb69a143bdd75c015b",
+    "url": "/beutiful-nails/static/media/s2.c47a9407.png"
+  },
+  {
+    "revision": "5929ee84f5f4cb52cb25f22147d1a951",
+    "url": "/beutiful-nails/static/media/s3.5929ee84.png"
+  },
+  {
+    "revision": "5b831307769018c73d1a7ede956c2014",
+    "url": "/beutiful-nails/static/media/s4.5b831307.png"
+  },
+  {
+    "revision": "97e76905035f956f5c4192aa601833d9",
+    "url": "/beutiful-nails/static/media/s5.97e76905.png"
+  },
+  {
+    "revision": "bbe04384d939ac8ffd3d9ace41d5bea0",
+    "url": "/beutiful-nails/static/media/s6.bbe04384.png"
+  },
+  {
+    "revision": "254de92011023dd00fe3da7ee7939376",
+    "url": "/beutiful-nails/static/media/s7.254de920.png"
+  },
+  {
+    "revision": "7bb4544f2e0dceee9417477c4559e1de",
+    "url": "/beutiful-nails/static/media/s8.7bb4544f.png"
+  },
+  {
+    "revision": "af87c316345beb872070353fa89c6e9b",
+    "url": "/beutiful-nails/static/media/s9.af87c316.png"
+  }
+]);
